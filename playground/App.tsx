937f94b2227ea17977179154611a2264feaebcb2
bigmistqke <vandijckv@gmail.com>
2025-08-02 16:41:51 +0200

    chore: move index.html and vite.config.ts to playground

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,20 +1,37 @@
-import { Component } from "solid-js";
-import * as THREE from "three";
-import { Vector3 } from "three";
-import { Canvas, T, extend } from "../src";
-import { Box } from "./Box";
 import "./index.css";
+import { createSignal } from "solid-js";
+import * as THREE from "three";
+import { Canvas, T, extend, useFrame } from "../src/index";
 
 extend(THREE);
 
-export const App: Component = () => {
-  // return "hallo";
+function Box() {
+  const mesh = new THREE.Mesh();
+  const [hovered, setHovered] = createSignal(false);
+
+  useFrame(() => (mesh!.rotation.y += 0.01));
+
   return (
-    <Canvas camera={{ position: new Vector3(0, 0, 5) }}>
+    <>
+      <T.Primitive
+        object={mesh}
+        onPointerEnter={e => setHovered(true)}
+        onPointerLeave={e => setHovered(false)}
+      >
+        <T.BoxGeometry />
+        <T.MeshStandardMaterial color={hovered() ? "green" : "red"} />
+      </T.Primitive>
+    </>
+  );
+}
+
+export function App() {
+  return (
+    <Canvas camera={{ position: new THREE.Vector3(0, 0, 5) }}>
       <T.AmbientLight color={[0.2, 0.2, 0.2]} />
       <T.PointLight intensity={1.2} decay={1} position={[2, 2, 5]} rotation={[0, Math.PI / 3, 0]} />
       <Box />
       <T.ExtrudeGeometry args={[[new THREE.Shape()]]} />
     </Canvas>
   );
-};
+}
